05ea478023b320e1efa5d08ac536c9c29f18b155
Leo Melin <leo.melin@gofore.com>
2018-08-14 07:40:45 +0300

    Remove some dead code

--- src/main/kotlin/org/webdevandsausages/backend/controller/HealthCheckController.kt
+++ src/main/kotlin/org/webdevandsausages/backend/controller/HealthCheckController.kt
@@ -1,19 +1,13 @@
 package org.webdevandsausages.backend.controller
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.webdevandsausages.backend.service.EventService
 
 
 @RestController
 @RequestMapping("/api/utils")
 class HealthCheckController {
-
-    @Autowired
-    lateinit var eventService: EventService
-
     @GetMapping("/health")
     fun healthCheck(): String {
         return "OK"
